901d6ec690183cedbf0017a1825a98a86ade2396
David Moles <david.moles@ucop.edu>
2015-12-22 12:02:54 -0800

    Sorted out scope/access issue

--- lib/factory/factory/abstract_factory.rb
+++ lib/factory/factory/abstract_factory.rb
@@ -8,7 +8,7 @@ module Factory
     end
 
     def build
-      build_from(config_hash)
+      self.class.build_from(config_hash)
     end
 
     def self.included(base)
@@ -22,33 +22,25 @@ module Factory
         if args.length == 1
           opts = args[0]
           k, v = opts.first
-          product_class = opts[:builds]
+          register(k, v, opts[:builds])
         else
           k = product_registry.keys.last
-          v = args[0]
-          opts = args[1]
-          product_class = opts[:builds]
+          register(k, args[0], args[1][:builds])
         end
+      end
 
+      def build_from(config_hash)
+        k, product = config_hash.lazy.map { |k, v| [k, product_for(k, v)] }.find { |r| r }
+        sub_config = config_hash.select { |k2, _v2| k2 != k }
+        product.new(sub_config)
+      end
+
+      private
+
+      def register(k, v, product_class)
         product_registry[k] ||= {}
         product_registry[k][v.to_s] = product_class
       end
-
-      # def builds(sym, opts)
-      #   @switch_symbol = sym
-      #   opts.each { |k, v| register_product(k, v) }
-      # end
-      #
-      # def register_product(switch_value, product_subclass)
-      #   fail ArgumentError, "Product #{product_registry[switch]} already registered for switch symbol '#{switch_symbol}'" if product_registry.key?(switch_symbol)
-      #   product_registry[switch_value] = product_subclass
-      # end
-      #
-      # def product_for(switch_symbol)
-      #   product_registry[switch_symbol]
-      # end
-
-      private
 
       def product_registry
         @product_registry ||= {}
@@ -59,14 +51,9 @@ module Factory
       end
 
       def product_for(k, v)
-        products = product_registry[k] and products[v.to_s]
+        (products = product_registry[k]) && products[v.to_s]
       end
 
-      def build_from(config_hash)
-        k, v, product = config_hash.lazy.map { |k, v| [k, v, product_for(k, v)] }.find { |r| r }
-        sub_config = config_hash.select { |k2, _v2| k2 != k }
-        product.new(sub_config)
-      end
     end
   end
 end
